95d133a91b979f9ea60c365e7606900b3e622d2b
XD3an <deanhs0dean@gmail.com>
2025-01-31 13:45:38 +0800

    update Transformer-decoder-only

---
+++ Transformer-decoder-only/src/evaluation.py
@@ -0,0 +1,95 @@
+import torch
+from transformers import pipeline, AutoTokenizer
+from datasets import load_dataset
+import json
+import evaluate
+
+from model import Model
+from utils import get_batch
+from generation import TextGenerator
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+class ModelConfig:
+    """Configuration for the model"""
+    CONTEXT_LENGTH: int = config['model']['context_length']     # Number of context frames (default: 16)
+    D_MODEL: int = config['model']['d_model']                   # Dimension of the model (must be divisible by NUM_HEADS)
+    D_FF: int = config['model']['d_ff']                         # Dimension of the feed forward network (must be equal to D_MODEL*4)
+    NUM_BLOCKS: int = config['model']['num_blocks']             # Number of transformer blocks in the model (default: 8)
+    NUM_HEADS: int = config['model']['num_heads']               # Number of heads in multi-head attention (must divide D_MODEL)
+    DROP_OUT: float = config['model']['dropout']                # Drop out rate for regularization (default: 0.1)
+    MODEL_PATH: str = config['train']['model_path']             # Path to save the model
+    DATASET_PATH: str = config['train']['dataset_path']         # Path to the dataset  
+    DEVICE: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def load_data(dataset_path: str):
+    """
+    Load the dataset
+
+    Args:
+        dataset_path (str): Path to the dataset
+        split (str): Split of the dataset
+
+    Returns:
+        prompts (list): List of prompts
+    """
+    dataset = load_dataset(dataset_path)
+    return dataset
+
+def evaluate_texts(generated_texts, reference_texts):
+    """
+    Evaluate the generated texts
+    
+    Args:
+        generated_texts (list): List of generated texts
+        reference_texts (list): List of reference texts
+
+    Returns:
+        bleu_score (dict): BLEU score
+        rouge_score (dict): ROGUE score
+        perplexity_score (dict): Perplexity score
+    """
+
+    # BLEU score
+    bleu = evaluate.load('bleu')
+    bleu_score = bleu.compute(predictions=generated_texts, references=reference_texts)
+
+    # ROGUE score
+    rouge = evaluate.load('rouge')
+    rouge_score = rouge.compute(predictions=generated_texts, references=reference_texts)
+
+    return {
+        'bleu': bleu_score, 
+        'rouge': rouge_score, 
+    }
+
+def main():
+
+    # test dataset: vesteinn/babylm    
+    model = Model().to(ModelConfig.DEVICE)
+    model.load_state_dict(torch.load(ModelConfig.MODEL_PATH))
+    model.eval()
+
+    # load the dataset
+    test_datasets = load_data(ModelConfig.DATASET_PATH)
+    print(f"Loaded {test_datasets} test datasets")
+
+    # load text generator
+    generator = TextGenerator(model_path=ModelConfig.MODEL_PATH)
+
+    # prepare the texts=pairs(generated_texts, reference_texts)
+    generated_texts = []
+    reference_texts = []
+    for dataset in test_datasets:
+        for prompt in dataset:
+            reference_texts.append(prompt)
+            generated_text = generator.generate_text(prompt)
+            generated_texts.append(generated_text)
+
+    # evaluate the model
+    scores = evaluate_texts(generated_texts, reference_texts)
+    print(f"BLEU score: {scores['bleu']}")
+    print(f"ROUGE score: {scores['rouge']}")
+
+if __name__ == "__main__":
+    main()
